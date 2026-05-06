ed6e5afa4486d32ef55554c7a0cce2bc9af3480e
Anis Ur Rahman <aniskhan25@gmail.com>
2026-05-06 18:03:07 +0300

    Added recent laif container release

--- env/lumi-env.sh
+++ env/lumi-env.sh
@@ -2,7 +2,7 @@
 
 export PROJECT_ACCOUNT="${PROJECT_ACCOUNT:-project_462000131}"
 export LUMI_USER="${LUMI_USER:-${USER:-anisrahm}}"
-export CONTAINER="${CONTAINER:-/appl/local/laifs/containers/lumi-multitorch-u24r64f21m43t29-20260225_144743/lumi-multitorch-full-u24r64f21m43t29-20260225_144743.sif}"
+export CONTAINER="${CONTAINER:-/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260415_130625/lumi-multitorch-full-u24r70f21m50t210-20260415_130625.sif}"
 
 export ANEMOI_DATA_ROOT="${ANEMOI_DATA_ROOT:-/scratch/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-demo/data}"
 export ANEMOI_GRAPH_ROOT="${ANEMOI_GRAPH_ROOT:-/project/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-demo/graphs}"
