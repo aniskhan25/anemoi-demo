89ecf33196f311ba08074c1843046802a5c845b8
Anis Ur Rahman <aniskhan25@gmail.com>
2026-07-01 10:49:04 +0300

    default to anemoi 0.14 container and baked-in venv

--- env/lumi-env.sh
+++ env/lumi-env.sh
@@ -2,9 +2,9 @@
 
 export PROJECT_ACCOUNT="${PROJECT_ACCOUNT:-project_462000131}"
 export LUMI_USER="${LUMI_USER:-${USER:-anisrahm}}"
-export CONTAINER="${CONTAINER:-/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260415_130625/lumi-multitorch-full-u24r70f21m50t210-20260415_130625.sif}"
+export CONTAINER="${CONTAINER:-/scratch/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-lumi.sif}"
 
 export ANEMOI_DATA_ROOT="${ANEMOI_DATA_ROOT:-/scratch/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-demo/data}"
 export ANEMOI_GRAPH_ROOT="${ANEMOI_GRAPH_ROOT:-/project/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-demo/graphs}"
 export ANEMOI_OUTPUT_ROOT="${ANEMOI_OUTPUT_ROOT:-/scratch/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-demo/logs}"
-export ANEMOI_VENV="${ANEMOI_VENV:-/scratch/${PROJECT_ACCOUNT}/${LUMI_USER}/anemoi-demo/.venv}"
+export ANEMOI_VENV="${ANEMOI_VENV:-/opt/anemoi-venv}"
